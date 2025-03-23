6c2f7a769c8a409c5d25711049268f7c3af40616
Kydo Solis <kydo.s@alphaonedesign.com>
2025-03-24 00:14:35 +0800

    iOS Initial Integration

--- ios/Classes/TuyaFlutterPlugin.swift
+++ ios/Classes/TuyaFlutterPlugin.swift
@@ -1,22 +1,142 @@
 import Flutter
 import UIKit
+import ThingSmartHomeKit
+import ThingSmartBusinessExtensionKit
+import ThingSmartCryption
 
 public class TuyaFlutterPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "tuya_flutter", binaryMessenger: registrar.messenger())
-    let instance = TuyaFlutterPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
-    case "initTuya":
-      ThingSmartSDK.sharedInstance().start(withAppKey: "YOUR_TUYA_APP_KEY", secretKey: "YOUR_TUYA_SECRET_KEY")
-      result("Tuya SDK Initialized on iOS")
-    default:
-      result(FlutterMethodNotImplemented)
+    private var channel: FlutterMethodChannel
+    private var context: UIApplication
+    private var meshEventChannel: FlutterEventChannel
+    private var meshEventSink: FlutterEventSink?
+    private static var activator: ThingSmartActivator?
+    
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "tuya_flutter", binaryMessenger: registrar.messenger())
+        let instance = TuyaFlutterPlugin(channel: channel, context: UIApplication.shared, messenger: registrar.messenger())
+        registrar.addMethodCallDelegate(instance, channel: channel)
+        
+        let meshEventChannel = FlutterEventChannel(name: "tuya_flutter/meshScanCallback", binaryMessenger: registrar.messenger())
+        meshEventChannel.setStreamHandler(instance)
     }
-  }
+    
+    init(channel: FlutterMethodChannel, context: UIApplication, messenger: FlutterBinaryMessenger) {
+        self.channel = channel
+        self.context = context
+        self.meshEventChannel = FlutterEventChannel(name: "tuya_flutter/meshScanCallback", binaryMessenger: messenger)
+        super.init()
+    }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "getPlatformVersion":
+            result("iOS " + UIDevice.current.systemVersion)
+        case "initTuya":
+            guard let args = call.arguments as? [String: Any],
+                  let appKey = args["appKey"] as? String,
+                  let appSecret = args["appSecret"] as? String else {
+                result(FlutterError(code: "NULL_VALUES", message: "App Key or Secret not found.", details: nil))
+                return
+            }
+            ThingSmartSDK.sharedInstance().start(withAppKey: appKey, secretKey: appSecret)
+            result("Tuya SDK Initialized")
+        case "loginWithEmail":
+            guard let args = call.arguments as? [String: Any],
+                  let countryCode = args["countryCode"] as? String,
+                  let email = args["email"] as? String,
+                  let passwd = args["passwd"] as? String else {
+                result(FlutterError(code: "MISSING_ARGS", message: "Missing parameters for login", details: nil))
+                return
+            }
+            ThingSmartUser.sharedInstance().login(byEmail: countryCode, email: email, password: passwd, success: {
+                self.channel.invokeMethod("loginCallback", arguments: ["status": "success", "message": "Login successful"])
+            }, failure: { error in
+                self.channel.invokeMethod("loginCallback", arguments: ["status": "error", "errorCode": error?.localizedDescription ?? "Unknown error"])
+            })
+            result("loginWithEmail initiated")
+        case "createHome":
+          result("Not implemented yet")
+        case "queryHomeList":
+          result("Not implemented yet")
+        case "getHomeDetail":
+          result("Not implemented yet")
+        
+        case "getDeviceList":
+          guard let args = call.arguments as? [String: Any],
+                let homeId = args["homeId"] as? Int64 else {
+              result(FlutterError(code: "MISSING_ARGS", message: "Missing homeId", details: nil))
+              return
+          }
+          if let home = ThingSmartHome(homeId: homeId) {
+              if let deviceList = home.deviceList {
+                  let devices = deviceList.map { device in
+                      return [
+                          "devId": device.devId,
+                          "name": device.name ?? ""
+                      ]
+                  }
+                  result(devices)
+              } else {
+                  result([])
+              }
+          } else {
+              result(FlutterError(code: "NO_HOME", message: "Unable to initialize home", details: nil))
+          }
+        case "getActivatorToken":
+            guard let args = call.arguments as? [String: Any], let homeId = args["homeId"] as? Int else {
+                result(FlutterError(code: "MISSING_ARGS", message: "Missing homeId", details: nil))
+                return
+            }
+            ThingSmartActivator().getTokenWithHomeId(Int64(homeId), success: { token in
+                result(token)
+            }, failure: { error in
+                result(FlutterError(code: "TOKEN_ERROR", message: error?.localizedDescription ?? "Unknown error", details: nil))
+            })
+        case "buildActivator":
+            guard let args = call.arguments as? [String: Any], let token = args["token"] as? String else {
+                result(FlutterError(code: "MISSING_ARGS", message: "Missing token", details: nil))
+                return
+            }
+            guard let ssid = args["ssid"] as? String, 
+                let password = args["password"] as? String else {
+                result(FlutterError(code: "MISSING_ARGS", message: "Missing ssid or password", details: nil))
+                return
+            }
+            let timeout = args["timeout"] as? Double ?? 100
+            TuyaFlutterPlugin.activator = ThingSmartActivator.sharedInstance()
+            TuyaFlutterPlugin.activator?.startConfigWiFi(.EZ, ssid: ssid, password: password, token: token, timeout: timeout)
+            result("Activator built successfully")
+        case "startActivator":
+            // TuyaFlutterPlugin.activator?.start()
+            result("Activator started")
+        case "stopActivator":
+            //TuyaFlutterPlugin.activator?.stop()
+            result("Activator stopped")
+        case "sendDpCommand":
+            guard let args = call.arguments as? [String: Any], let devId = args["devId"] as? String, let dpId = args["dpId"] as? String, let dpValue = args["dpValue"] as? Int else {
+                result(FlutterError(code: "MISSING_ARGS", message: "Missing devId or dpId", details: nil))
+                return
+            }
+            let device = ThingSmartDevice(deviceId: devId)
+            device?.publishDps([dpId: dpValue == 1], success: {
+                result("Command sent successfully")
+            }, failure: { error in
+                result(FlutterError(code: "SEND_DP_ERROR", message: error?.localizedDescription ?? "Unknown error", details: nil))
+            })
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
+}
+
+extension TuyaFlutterPlugin: FlutterStreamHandler {
+    public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
+        self.meshEventSink = events
+        return nil
+    }
+
+    public func onCancel(withArguments arguments: Any?) -> FlutterError? {
+        self.meshEventSink = nil
+        return nil
+    }
 }
--- ios/tuya_flutter.podspec
+++ ios/tuya_flutter.podspec
@@ -1,29 +1,35 @@
-#
-# To learn more about a Podspec see http://guides.cocoapods.org/syntax/podspec.html.
-# Run `pod lib lint tuya_flutter.podspec` to validate before publishing.
-#
 Pod::Spec.new do |s|
   s.name             = 'tuya_flutter'
   s.version          = '0.0.1'
-  s.summary          = 'A new Flutter plugin project.'
-  s.description      = <<-DESC
-A new Flutter plugin project.
-                       DESC
-  s.homepage         = 'http://example.com'
+  s.summary          = 'Flutter plugin for Tuya Smart SDK'
+  s.description      = 'Flutter plugin wrapper for Tuya Smart Home SDK.'
+  s.homepage         = 'http://www.alphaonedesign.com'
   s.license          = { :file => '../LICENSE' }
-  s.author           = { 'Your Company' => 'email@example.com' }
+  s.author           = { 'Alpha One Design' => 'info@alphaonedesign.com' }
   s.source           = { :path => '.' }
-  s.source_files = 'Classes/**/*'
-  s.dependency 'Flutter'
-  s.platform = :ios, '12.0'
-
-  # Flutter.framework does not contain a i386 slice.
-  s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES', 'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'i386' }
+  s.source_files     = 'Classes/**/*'
+  s.public_header_files = 'Classes/**/*.h'
+  
+  s.platform = :ios, '15.5'
   s.swift_version = '5.0'
 
-  # If your plugin requires a privacy manifest, for example if it uses any
-  # required reason APIs, update the PrivacyInfo.xcprivacy file to describe your
-  # plugin's privacy impact, and then uncomment this line. For more information,
-  # see https://developer.apple.com/documentation/bundleresources/privacy_manifest_files
-  # s.resource_bundles = {'tuya_flutter_privacy' => ['Resources/PrivacyInfo.xcprivacy']}
+  # Flutter dependency
+  s.dependency 'Flutter'
+
+  # Tuya SDK Dependencies (Hihiramin mula sa main app)
+  s.dependency 'ThingSmartCryption'
+  s.dependency 'ThingSmartHomeKit'
+  s.dependency 'ThingSmartBusinessExtensionKit'
+
+  # Ensure it can find the Tuya SDK in the consuming app
+  s.xcconfig = {
+    'FRAMEWORK_SEARCH_PATHS' => '$(inherited) "$(PODS_ROOT)/ThingSmartCryption/**" "$(PODS_ROOT)/ThingSmartHomeKit/**" "$(PODS_ROOT)/ThingSmartBusinessExtensionKit/**"',
+    'IPHONEOS_DEPLOYMENT_TARGET' => '15.5'
+  }
+
+  # Ensure compatibility with iOS Simulators
+  s.pod_target_xcconfig = { 
+    'DEFINES_MODULE' => 'YES', 
+    'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'i386 arm64' 
+  }
 end
